b57753a1398e4b062f399f8f4b26b7ac483c1968
acimisk <acimisk@gmail.com>
2025-05-24 01:19:53 +0300

    Eğer posts limit dışına çıkarsa 404 statusu verecek ve kullanıcıya hata mesajı gösterecek

--- server.js
+++ server.js
@@ -12,8 +12,28 @@ let posts = [
     {id:3, title: 'Post3'},
 ];
 
+// Get all posts
 app.get('/api/posts', (req,res) => {
+    const limit = parseInt(req.query.limit);
+
+    if(!isNaN(limit) && limit > 0) {
+        res.status(200).json(posts.slice(0,limit));
+    }   else {
+        res.status(200).json(posts);
+    }
+
     res.json(posts);
 })
 
+// Get single post
+app.get('/api/posts/:id', (req,res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post)=> post.id === id);
+
+    if(!post) {
+        res.status(404).json({msg: `A post witht the id of ${id} was not found`});    
+    } else {
+        res.status(200).json(post);
+    }
+})
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
