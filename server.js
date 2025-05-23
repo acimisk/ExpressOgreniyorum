c2248387ef7b7c770821a2f29db0898e69fd162d
acimisk <acimisk@gmail.com>
2025-05-24 01:08:00 +0300

    Artık PORT'u .env dosyasından çekiyoruz

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require ('path');
 const app = express();
 
+const PORT = process.env.PORT;
 // setup static folder
 //app.use(express.static(path.join(__dirname, 'public')));
 
@@ -15,4 +16,4 @@ app.get('/api/posts', (req,res) => {
     res.json(posts);
 })
 
-app.listen(8000, () => console.log(`Server is running on port 8000`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
