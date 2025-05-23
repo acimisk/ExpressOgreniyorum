6ffd51a4b7c11aa78451d2d5a1544b36a52726d8
acimisk <acimisk@gmail.com>
2025-05-24 02:12:51 +0300

    Logger middleware yazıldı

--- routes/posts.js
+++ routes/posts.js
@@ -7,8 +7,13 @@ let posts = [
     {id:3, title: 'Post3'},
 ];
 
+const logger = (req ,res ,next) => { 
+    console.log(
+    `${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+    next();
+};
 // Get all posts
-router.get('/', (req,res) => {
+router.get('/', logger, (req,res) => {
     const limit = parseInt(req.query.limit);
 
     if(!isNaN(limit) && limit > 0) {
@@ -16,8 +21,6 @@ router.get('/', (req,res) => {
         .status(200).json(posts.slice(0,limit));
     }   
         res.status(200).json(posts);
-
-    res.json(posts);
 });
 
 // Get single post
@@ -46,6 +49,38 @@ router.post('/', (req,res) => {
     res.status(201).json(posts);
 });
 
+// Update Post
+router.put('/:id', (req,res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+
+    if(!post) {
+        return res
+        .status(404)
+        .json({msg:`A post with the id of ${id} was not found`})
+    }
+
+    post.title = req.body.title;
+    res.status(200).json(posts);
+
+});
+
+// Delete Post
+
+router.delete('/:id', (req,res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+
+    if(!post) {
+        return res
+        .status(404)
+        .json({msg:`A post with the id of ${id} was not found`})
+    }
+
+    posts = posts.filter((post) => post.id !== id);
+    res.status(200).json(posts);
+
+});
 
 
 export default router;
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import path from 'path';
 import posts from './routes/posts.js';
-
+import logger from './middleware/logger.js';
 const app = express();
 const PORT = process.env.PORT;
 
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+// Logger middleware
+app.use(logger);
 
 // setup static folder
 //app.use(express.static(path.join(__dirname, 'public')));
