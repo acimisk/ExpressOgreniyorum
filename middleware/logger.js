9b5062778699845528db9cfec1e38dd05e8206af
acimisk <acimisk@gmail.com>
2025-05-24 16:35:27 +0300

    feat: add colored request logger middleware

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,6 +1,21 @@
+import colors from 'colors';
+
+
 const logger = (req ,res ,next) => { 
-    console.log(
+    const methodColors = {
+        GET: 'green',
+        POST: 'blue',
+        PUT: 'yellow',
+        DELETE: 'red'
+    };
+    
+    const color = methodColors[req.method] || 'white';
+
+    const coloredText = colors[ color ](
     `${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+    
+    console.log(coloredText);
+
     next();
 };
 
