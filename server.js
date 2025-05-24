d5463b4f6907853d917cc10e3f70157ac70b8832
acimisk <acimisk@gmail.com>
2025-05-24 21:58:14 +0300

    Temelleri öğrendim proje bitti.

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import posts from './routes/posts.js';
 import logger from './middleware/logger.js';
 import errorHandler from './middleware/error.js';
 import notFound from './middleware/notFound.js';
+
+// Get the directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 
 const app = express();
 const PORT = process.env.PORT;
@@ -16,6 +23,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(logger);
 
 // setup static folder
+app.use(express.static(path.join(__dirname, 'public')));
 //app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
